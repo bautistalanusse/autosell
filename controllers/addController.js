9af6e33680972ad65a075ad70e896eae9f0a44c3
nachocavanagh <icavanaghnicholson@udesa.edu.ar>
2021-05-25 19:22:31 -0300

    product add

--- controllers/addController.js
+++ controllers/addController.js
@@ -1,6 +1,22 @@
+const bcrypt = require('bcryptjs')
+const db = require('../database/models')
+const op = db.Sequelize.Op;
+
 let addController = {
-    index: function (req, res, next) {
-        res.render('product-add');
+    index: function (req, res) {
+        if (req.method == 'POST') {
+            db.Product.create(req.body)
+                .then(() => {
+                    res.redirect('/')
+                })
+                .catch((error) => {
+                    return res.send(error);
+                })
+        }
+        if (req.method == 'GET') {
+            return res.render('product-add')
+
+        }
     },
 }
 
